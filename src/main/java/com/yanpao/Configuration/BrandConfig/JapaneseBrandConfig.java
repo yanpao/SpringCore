41af4cc482f943863ab670ff6610ef144bcb138c
PC-20200728PRED\Administrator <lihanzhang@outlook.com>
2021-01-11 17:24:41 +0800

    测试autowire，但是有些依赖问题还没解决，比如根据类型还是名字

--- src/main/java/com/yanpao/Configuration/BrandConfig/JapaneseBrandConfig.java
+++ src/main/java/com/yanpao/Configuration/BrandConfig/JapaneseBrandConfig.java
@@ -4,7 +4,6 @@ import com.yanpao.Cars.Brand;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JapaneseBrandConfig {
